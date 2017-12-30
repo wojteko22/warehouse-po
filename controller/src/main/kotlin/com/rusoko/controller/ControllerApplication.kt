e58f373932b7d73a754b41ac4fbb7fdf30c5a2df
Dawid Rusiecki <dawid.rusiecki@fingo.info>
2017-12-30 09:42:39 +0100

    First commit

---
+++ controller/src/main/kotlin/com/rusoko/controller/ControllerApplication.kt
@@ -0,0 +1,11 @@
+package com.rusoko.controller
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ControllerApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(ControllerApplication::class.java, *args)
+}
---
+++ controller/src/test/kotlin/com/rusoko/controller/ControllerApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.rusoko.controller
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ControllerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
