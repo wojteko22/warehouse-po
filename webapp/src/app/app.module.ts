18e307d7bb12dccbaf80df4f5e3f20069bf155b4
Dawid Rusiecki <dawid.rusiecki@fingo.info>
2018-01-11 01:52:11 +0100

    FRONT - small reformat

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
 import {CoreModule} from "./core-module/core.module";
 import {SharedModule} from "./shared-module/shared.module";
@@ -21,4 +21,5 @@ import {SharedModule} from "./shared-module/shared.module";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
