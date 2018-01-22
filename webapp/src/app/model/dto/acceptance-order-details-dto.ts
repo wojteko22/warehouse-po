7914cbe27ba51b8ae976f6cf93c56bb6d5a71365
Wojtek Okoński <wojteko22.96@gmail.com>
2018-01-22 08:04:28 +0100

    Fix acceptance order status

--- webapp/src/app/model/dto/acceptance-order-details-dto.ts
+++ webapp/src/app/model/dto/acceptance-order-details-dto.ts
@@ -1,5 +1,6 @@
 import {AcceptanceOrderPositionDto} from "./acceptance-order-position-dto";
 
 export interface AcceptanceOrderDetailsDto {
+  status: number,
   positions: AcceptanceOrderPositionDto[]
 }
