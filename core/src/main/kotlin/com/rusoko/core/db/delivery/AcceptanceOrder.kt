e8e348e262187e703c17a803ac74ea87e8d2b7cd
Wojtek Okoński <wojteko22.96@gmail.com>
2018-01-22 01:02:59 +0100

    Add acceptanceOrder

--- core/src/main/kotlin/com/rusoko/core/db/delivery/AcceptanceOrder.kt
+++ core/src/main/kotlin/com/rusoko/core/db/delivery/AcceptanceOrder.kt
@@ -4,10 +4,13 @@ import com.rusoko.api.dto.AcceptanceOrderDto
 import com.rusoko.core.connect
 import com.rusoko.core.db.InitializableTable
 import com.rusoko.core.new
+import com.rusoko.core.repositories.DifferenceReportRepositoryImpl
 import org.jetbrains.exposed.dao.EntityID
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main(args: Array<String>) {
     connect {
@@ -20,7 +23,7 @@ class AcceptanceOrder(id: EntityID<Int>) : IntEntity(id) {
 
     var differenceReport by DifferenceReport referencedOn AcceptanceOrders.differenceReport
 
-    private val deliveryOrder = differenceReport.deliveryOrder
+    private val deliveryOrder get() = differenceReport.deliveryOrder
 
     fun toDto() = AcceptanceOrderDto(id.value, deliveryOrder.orderNumber, deliveryOrder.provider.name)
 }
@@ -30,5 +33,12 @@ object AcceptanceOrders : InitializableTable("acceptance_orders") {
 
     override fun init() {
         SchemaUtils.new(this)
+        val deliveryOrderId = transaction { DeliveryOrders.insertAndGetId().value }
+        val differenceReportRepository = DifferenceReportRepositoryImpl()
+        val differenceReportId = differenceReportRepository.createDefault(deliveryOrderId)
+        differenceReportRepository.send(differenceReportId)
+        AcceptanceOrders.insert {
+            it[differenceReport] = EntityID(differenceReportId, DifferenceReports)
+        }
     }
 }
