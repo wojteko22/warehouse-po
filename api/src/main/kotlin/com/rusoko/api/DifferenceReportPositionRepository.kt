9d9388f912166670faf8653b457629d17c75c2ef
Dawid Rusiecki <dawid.rusiecki@fingo.info>
2018-01-22 07:55:16 +0100

    DOC - difference report position repository interface

--- api/src/main/kotlin/com/rusoko/api/DifferenceReportPositionRepository.kt
+++ api/src/main/kotlin/com/rusoko/api/DifferenceReportPositionRepository.kt
@@ -2,7 +2,15 @@ package com.rusoko.api
 
 import java.math.BigDecimal
 
+/**
+ * Repository of difference report positions allowing to cooperate with data base
+ */
 interface DifferenceReportPositionRepository {
 
+    /**
+     * Save delivered quantity of commodity from report
+     * @param positionId id of position from report
+     * @param deliveredQuantity delivered quantity of the commodity
+     */
     fun savePosition(positionId: Int, deliveredQuantity: BigDecimal)
 }
